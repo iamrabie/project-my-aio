06919c59e2cc53d178edef947f1df3e61bc85342
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-01 17:30:02 +0500

    imported Title

--- my-aio-clone/src/App.js
+++ my-aio-clone/src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
+import "./App.css";
 import Title from "./components/Title/title";
 
 function App() {
   return (
     <div className="App">
- <Title text="Make Decisions" size="large" />
- <Title text="Preductive Analysis" size="small" />
-
+      <Title text="Make Decisions 10X Effective with My AIO" size="large" highlightedText="10X Effective" />
+      <Title text="Getting to Know MY AIO" size="small" highlightedText="aio" />
     </div>
   );
 }
