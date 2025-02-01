b501e20cd9df8d5b75f35667af783988b2b9fe5d
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-02 02:39:49 +0500

    bb

--- my-aio-clone/src/components/IrregularCard/irregularCard.jsx
+++ my-aio-clone/src/components/IrregularCard/irregularCard.jsx
@@ -1,34 +1,27 @@
 import '../../styles/irregular.css';
+import Title from '../Title/title';
+import { LuBrain } from "react-icons/lu";
+import ReactFlipCard from 'reactjs-flip-card'
+
 
 
 const IrregularCard = () => {
-    return(
-        <>
-        <div className='inverted-radius p-5'>
-            {/* <div className='border'> */}
-<p>bnm, nnnn
-bnm, nn
-bnm, nn
-bnm, n bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</p>
-<p>
-bnm, nnnn
-bnm, nn
-bnm, nn
-bnm, nbnm, nnnn
-bnm, nn
-</p>
-
-<p>
-bnm, nn
-bnm, nbnm, nnnn
-bnm, nn
-bnm, nn
-bnm, n   </p>
-{/* 
-            </div> */}
+  return (
+    <>
+    {/* <div className='flex'> */}
+      <div className='border w-[fit-content] relative top-[69px] left-[6px] bg-green-700 rounded-full p-3.5 text-3xl border border-[#160042] bg-[#3f2581] text-white'><LuBrain />
+      </div>
+      <div className='inverted-radius ps-[84px] flex justify-center items-center'>
+        <div className='flex flex-col gap-y-2.5 w-[fit-content] px-4 text-[15px] justify-center'>
+            <Title text="Predictive Analytics with GPT-4" size="small" />
+            <p className='leading-6'>
+            MY AIO use the cutting-edge GPT-4 AI model to provide the most accurate insights that anticipate market trends, giving you a competitive edge.
+            </p>
         </div>
-        </>
-    );
-}
+      </div>
+    {/* </div> */}
+    </>
+  );
+};
 
 export default IrregularCard;
