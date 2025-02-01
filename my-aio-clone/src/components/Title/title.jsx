000e376a7a3cde657aad4e0cc7e85b43914eacaf
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-01 17:28:56 +0500

    created reusable heading comp for larger and smaller headings.

--- my-aio-clone/src/components/Title/title.jsx
+++ my-aio-clone/src/components/Title/title.jsx
@@ -1,18 +1,28 @@
-const Title = ({color , size , text}) => {
+const Title = ({ size, text, highlightedText }) => {
+  //title size
+  const titleSize = {
+    small: "text-xl",
+    large: "text-6xl",
+  };
 
-    const titleSize = {
-        small:'text-xl',
-        large:'text-6xl'
-    }
+  //for highlighted Text
+  const parts = text.split(new RegExp(`(${highlightedText})`, "gi"));
 
-
-    console.log('size :'  , titleSize[size]);
-
-    return(
-        <>
-        <p className={`p-0 m-0 ${titleSize[size]}`}>{text}</p>
-        </>
-    );
-}
+  return (
+    <>
+      <p className={`p-0 m-0 font-bold ${titleSize[size]}`}>
+        {parts.map((part, index) =>
+          part.toLowerCase() === highlightedText?.toLowerCase() ? (
+            <span key={index} className="text-[#0d6efd]">
+              {part}
+            </span>
+          ) : (
+            part
+          )
+        )}
+      </p>
+    </>
+  );
+};
 
 export default Title;
