d2ff1cbb325716b19e720d55f70d42f323535f0e
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-01 18:10:36 +0500

    gh

--- my-aio-clone/src/components/Title/title.jsx
+++ my-aio-clone/src/components/Title/title.jsx
@@ -10,7 +10,7 @@ const Title = ({ size, text, highlightedText }) => {
 
   return (
     <>
-      <p className={`p-0 m-0 font-bold ${titleSize[size]}`}>
+      <p className={`p-0 m-0 font-bold text-white ${titleSize[size]}`}>
         {parts.map((part, index) =>
           part.toLowerCase() === highlightedText?.toLowerCase() ? (
             <span key={index} className="text-[#0d6efd]">
