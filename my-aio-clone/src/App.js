ef67776439132df9b297ace50b3c2d3bb5bdb9b0
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-01 17:42:39 +0500

    added bg

--- my-aio-clone/src/App.js
+++ my-aio-clone/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Title text="Make Decisions 10X Effective with My AIO" size="large" highlightedText="10X Effective" />
-      <Title text="Getting to Know MY AIO" size="small" highlightedText="aio" />
+      <Title text="Getting to Know MY AIO" size="small" />
     </div>
   );
 }
