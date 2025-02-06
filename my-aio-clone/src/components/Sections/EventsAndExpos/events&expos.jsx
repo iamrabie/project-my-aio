374d09e58775f92285de11f10a608f68e5675ae4
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-07 02:43:00 +0500

    listed all sections in about page

---
+++ my-aio-clone/src/components/Sections/EventsAndExpos/events&expos.jsx
@@ -0,0 +1,20 @@
+import Title from "../../Title/title";
+
+const EventsAndExpos = () => {
+    return(
+        <>
+        <Title text="Events & Expos" size="large" highlightedText="Expos" />
+        <div className="flex">
+            <div className="w-[50%]">
+
+            </div>
+            <div className="w-[50%]">
+
+            </div>
+        </div>
+        </>
+    );
+}
+
+
+export default EventsAndExpos;
--- my-aio-clone/src/routes/About/about.jsx
+++ my-aio-clone/src/routes/About/about.jsx
@@ -11,6 +11,7 @@ import {
 } from "../../data/data";
 import Title from "../../components/Title/title";
 import Contact from "../../components/Contact/contact";
+import EventsAndExpos from "../../components/Sections/EventsAndExpos/events&expos";
 // import IrregularCard from "../../components/IrregularCard/irregularCard";
 
 const About = () => {
@@ -33,20 +34,41 @@ const About = () => {
           <MakeDecisions />
         </SectionCard>
 
+        {/* EVENTS AND EXPOS SECTION */}
+        <SectionCard display="flex flex-col" gapY="gap-y-8">
+           <EventsAndExpos />
+        </SectionCard>
+
+        {/* MAKE DATA SMARTER SECTION */}
+        <SectionCard display="flex flex-col" gapY="gap-y-8">
+           <Title text="Making Data Smarter & Simpler" size="large" highlightedText="Smarter" />
+        </SectionCard>
+
+
+        {/* Getting to Know */}
+        <SectionCard>
+          <GettingToKnow />
+        </SectionCard>
+
+        {/* SUCCESS STORIES SECTION */}
+        <SectionCard>
+        <Title text="Our Success Stories" size="large" highlightedText="Success" />
+        </SectionCard>
+
         {/* OVERALL RATING SECTION */}
         <SectionCard display="flex flex-col" gapY="gap-y-10">
           <OverallRating />
         </SectionCard>
 
+        <SectionCard>
+         <Title text="Professionals & Teams Choose Us" size="large" highlightedText="Choose Us" />
+        </SectionCard>
+
         {/* CONTACT US SECTION */}
         <SectionCard display="flex flex-col" gapY="gap-y-20" width="w-[80%]">
            <Contact />
-         </SectionCard>
-         
-        {/* Getting to Know */}
-        <SectionCard>
-          <GettingToKnow />
         </SectionCard>
+         
       </div>
     </>
   );
