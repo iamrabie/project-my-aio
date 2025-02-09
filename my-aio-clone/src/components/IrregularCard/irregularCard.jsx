9a304aff87a6845249b95451db011aa70906a274
Rabia Khan <106153809+iamrabie@users.noreply.github.com>
2025-02-09 22:50:35 +0500

    added flip animation on scroll on irregular card icon and translate on the rorregular cards.

--- my-aio-clone/src/components/IrregularCard/irregularCard.jsx
+++ my-aio-clone/src/components/IrregularCard/irregularCard.jsx
@@ -20,10 +20,10 @@ const IrregularCard = ({ icon, title, description, size }) => {
   return (
     <>
       {/* icon container */}
-      <div className={`${iconStyles[size]} bg-fabricOfSpace animate-hflip`}>{icon}</div>
+      <div className={`${iconStyles[size]} bg-fabricOfSpace border-2 border-cyan-700`} data-aos="flip-left" data-aos-duration="800">{icon}</div>
       {/* irregualr shape container */}
       <div
-        className={`${cardStyles[size]} bg-gradient-to-r from-purple-900/20 shadow shadow-sm to-blue-900/20 flex justify-center items-center border border-[#FFFFFF14]`}
+        className={`${cardStyles[size]} bg-gradient-to-r from-purple-900/20 shadow shadow-sm to-blue-900/20 flex justify-center items-center border border-[#FFFFFF14] hover:-translate-y-2 transition duration-300 ease-in-out`}
       >
         <div className={`${innerCardStyles[size]} text-driftingDream`}>
           <Title text={title} size="small" />
